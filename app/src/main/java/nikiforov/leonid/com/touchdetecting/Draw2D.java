917399f3c7da5a92fec95b216ca3b9bfa3b53a98
Jokeron <abaahi@gmail.com>
2016-04-21 16:07:44 +0900

    куча измененений

--- app/src/main/java/nikiforov/leonid/com/touchdetecting/Draw2D.java
+++ app/src/main/java/nikiforov/leonid/com/touchdetecting/Draw2D.java
@@ -5,7 +5,9 @@ import android.graphics.Bitmap;
 import android.graphics.Canvas;
 import android.graphics.Color;
 import android.graphics.Paint;
+import android.graphics.Path;
 import android.graphics.Rect;
+import android.view.MotionEvent;
 import android.view.View;
 
 /**
@@ -14,13 +16,22 @@ import android.view.View;
 public class Draw2D extends View {
     public Draw2D(Context context){
         super(context);
-        int canvasWidth;
-        int canvasHeight;
+
     }
     private Paint mPaint = new Paint();
     private Rect mRect = new Rect();
     int canvasWidth;
     int canvasHeight;
+    Path path = new Path();
+    //координаты круга
+    float circleX;
+    float circleY;
+
+    //координаты для рисования другого круга
+    float circleX1;
+    float circleY1;
+
+
 
 
     @Override
@@ -38,35 +49,55 @@ public class Draw2D extends View {
         mPaint.setColor(Color.WHITE);
         canvas.drawPaint(mPaint);
 
-        //рисуем солнце
-        mPaint.setAntiAlias(true);
-        mPaint.setColor(Color.YELLOW);
-        canvas.drawCircle(Math.round(canvasWidth*0.9), Math.round(canvasHeight*0.1), Math.round(canvasHeight*0.05), mPaint);
-
         //рисуем лужайку
         mPaint.setColor(Color.GREEN);
-        canvas.drawRect(20, 650, 950, 680, mPaint);
+        canvas.drawRect(20, Math.round(canvasHeight * 0.9), canvasWidth - 20, canvasHeight, mPaint);
 
         //рисуем небо
         mPaint.setColor(Color.BLUE);
-        canvas.drawRect(20, 20, 950, 650, mPaint);
+        canvas.drawRect(20, 20, canvasWidth-20, Math.round(canvasHeight*0.9), mPaint);
+
+        //рисуем солнце
+        mPaint.setAntiAlias(true);
+        mPaint.setColor(Color.YELLOW);
+        canvas.drawCircle(Math.round(canvasWidth - canvasHeight*0.1), Math.round(canvasHeight * 0.1), Math.round(canvasHeight * 0.05), mPaint);
 
         //рисуем надпись
         mPaint.setColor(Color.WHITE);
-        mPaint.setTextSize(32);
-        canvas.drawText("Лужайка только для котов!", 30, 648, mPaint);
+        mPaint.setTextSize(50);
+        canvas.drawText("Лужайка только для котов!", 30, Math.round(canvasHeight*0.85), mPaint);
 
         //рисуем наклонную надпись
         mPaint.setColor(Color.YELLOW);
-        mPaint.setTextSize(27);
+        mPaint.setTextSize(35);
         String sunLights = "Лучи солнца";
-        int x = 890;
-        int y = 190;
+        int x = (int)Math.round(canvasWidth*0.65);
+        int y = (int)Math.round(canvasHeight*0.35);
         canvas.rotate(-45, x + mRect.exactCenterX(), y + mRect.exactCenterY());
-        canvas.drawText(sunLights,x,y,mPaint);
+        canvas.drawText(sunLights, x, y, mPaint);
 
         //восстанавливаем холст
         canvas.restore();
+
+        //пишем текст по линии
+        path.reset();
+        path.moveTo(Math.round(canvasWidth - (canvasHeight * 0.35)), Math.round(canvasHeight * 0.35));
+        path.lineTo(Math.round(canvasWidth - canvasHeight * 0.1), Math.round(canvasHeight * 0.1));
+        canvas.drawTextOnPath("Лучи солнца!!!!!", path, 0, 0, mPaint);
+
+        //рисуем круг
+        mPaint.setColor(Color.WHITE);
+        circleX = (float)(canvasWidth*0.5);
+        circleY = (float)(canvasHeight*0.5);
+        path.reset();
+        path.addCircle(circleX,circleY,100, Path.Direction.CW);
+
+
+
     }
 
+    @Override
+    public boolean onTouchEvent(MotionEvent event) {
+        return super.onTouchEvent(event);
+    }
 }
--- app/src/main/java/nikiforov/leonid/com/touchdetecting/MainActivity.java
+++ app/src/main/java/nikiforov/leonid/com/touchdetecting/MainActivity.java
@@ -2,15 +2,18 @@ package nikiforov.leonid.com.touchdetecting;
 
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
+import android.widget.RelativeLayout;
 
-public class MainActivity extends AppCompatActivity {
+public class MainActivity extends AppCompatActivity  {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
-        //setContentView(R.layout.activity_main);
+        setContentView(R.layout.activity_main);
         Draw2D draw2D = new Draw2D(this);
-        setContentView(draw2D);
+        setContentView(R.layout.activity_main);
+        RelativeLayout relativeLayout = (RelativeLayout)findViewById(R.id.mainRelativeLayout);
+        relativeLayout.addView(draw2D);
 
 
 
